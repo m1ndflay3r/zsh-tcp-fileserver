ea16e224c27ef55d5f4d58b881bcc872089a9c8c
Vanity <c1ph3r8472@gmail.com>
2022-09-28 07:20:14 -0600

    misc

--- server.zsh
+++ server.zsh
@@ -2,45 +2,48 @@
 
 zmodload zsh/net/tcp
 AUTHORIZED="0"
-# In case anyone ends up playing with this before I have a chance to put more work into this
+# In case anyone ends up playing with this before I have a chance to put more work into it
 # You have to put your pubkey in here as well at the moment, as comparison against privatekey hasnt been implemented yet
 # If you search the string 'insert-key-here' youll find it
 main() {
   ztcp -l 333
   fd=$REPLY
   echo "Waiting for connection..."
-  echo "listening on port 333    $(date +"%T")" >> /root/testing/servroot/server.log
+  echo "$(date +"%T")    listening on port 333" >> /root/testing/servroot/server.log
   ztcp -a $fd
   clientfd=$REPLY
   echo "Client connected"
-  echo "client connected    $(date +"%T")" >> /root/testing/servroot/server.log
-  echo "$(tput setaf 2)Attempting to authenticate session...$(tput sgr 0)" >& $clientfd
+  echo "$(date +"%T")    client connected" >> /root/testing/servroot/server.log
+  echo "$(tput setaf 2)Authenticating session...$(tput sgr 0)" >& $clientfd
   LOOPY="0"
   while [ $LOOPY = 0 ]; do
     if ! read -t 600 line <& $clientfd; then
       echo "$(tput setaf 5)No response from client in 600 seconds! $(tput sgr 0)" >& $clientfd
+      echo "$(date +"%T")    connection closed - client timed out " >> /root/testing/servroot/server.log
       line="close"
     fi
     IPLISTEN=${line:0:5}
     if [[ $IPLISTEN = "setip" ]]; then
       if [ $AUTHORIZED = "0" ]; then
         CLIENTIP=${line:5}
-        echo "client reported ip address $CLIENTIP    $(date +"%T")" >> /root/testing/servroot/server.log
-        echo "Confirming authkey is valid..." >& $clientfd
+        echo "$(date +"%T")    client reported ip address $CLIENTIP" >> /root/testing/servroot/server.log
+        echo "Checking authkey..." >& $clientfd
         read -t 600 line <& $clientfd
         if [[ "$line" = 'insert-key-here' ]]; then
           SHPASS=1
           MDPASS=1
         fi
         if [ $SHPASS = 0 ] || [ $MDPASS = 0 ]; then
-          echo "$(tput setaf 1) identity key varification failure $(tput sgr 0)"
+          echo "$(tput setaf 1) identity key verification failure $(tput sgr 0)" >& $clientfd
+          echo "$(date +"%T")    connection closed - bad pubkey" >> /root/testing/servroot/server.log
           line="close"
         fi
         ISIPSEND="1"
         AUTHORIZED="1"
-        echo "$(tput setaf 2) authorization succesful$(tput sgr 0)" >& $clientfd
+        echo "$(tput setaf 2) authorization successful$(tput sgr 0)" >& $clientfd
+        echo "$(date +"%T")    authentication successful" >> /root/testing/servroot/server.log
       else
-        echo "client tried to report ip address twice    $(date +"%T")" >> /root/testing/servroot/server.log
+        echo "$(date +"%T")    client tried to report ip address twice" >> /root/testing/servroot/server.log
         echo "$(tput setaf 1) trying to pull some tomfuckery are we?$(tput sgr 0)" >& $clientfd
       fi
     else
@@ -54,7 +57,7 @@ main() {
       REQUESTPATH=${REQUESTPATH//../.}
       TOBESERVED="$REQUESTPATH/$REQUESTFILE"
       echo "Received request for file $line"
-      echo "client requested $line    $(date +"%T")" >> /root/testing/servroot/server.log
+      echo "$(date +"%T")    client requested $line" >> /root/testing/servroot/server.log
       if [[ -f "/root/testing/servroot/$TOBESERVED" ]]; then
         IFS=$'\n'
         DODODODO=($(cat /root/testing/servroot/$TOBESERVED))
@@ -70,11 +73,12 @@ main() {
         echo "bigrip" >& $clientfd
       fi
     elif [ $ISIPSEND = 0 ] && [ $AUTHORIZED = 0 ]; then
-      echo "$(tput setaf 5)access denied - trying to pull some tomfuckery, are we?" >& $clientfd
+      echo "$(tput setaf 5)access denied" >& $clientfd
+      echo "$(date +"%T")    connection closed - access denied" >> /root/testing/servroot/server.log
     fi
   done
   echo "Client disconnected"
-  echo "connection closed    $(date +"%T")" >> /root/testing/servroot/server.log
+  echo "$(date +"%T")    connection closed - client disconnect" >> /root/testing/servroot/server.log
   ztcp -c $fd
   ztcp -c $clientfd
   AUTHORIZED="0"
